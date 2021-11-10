139f7890498770cfd0ba70b6c4468763575de83a
Gruber <luis.gruber@aau.at>
2021-11-10 14:30:40 +0100

    Initial commit

---
+++ R/aux_functions.R
@@ -0,0 +1,148 @@
+
+
+# density of Dirichlet  ---------------------------------------------------
+
+ddir <- function(x, alpha, log = FALSE) {
+  if(!is.matrix(x)) x <- rbind(x)
+  if(!is.matrix(alpha)) {
+    alpha <- matrix(alpha, nrow = nrow(x), ncol = length(alpha), byrow = TRUE)
+  }
+  logd <- as.vector(rowSums((alpha - 1) * log(x)) + lgamma(rowSums(alpha)) - rowSums(lgamma(alpha)))
+  if(log == TRUE) out <- logd else if(log == FALSE) out <- exp(logd)
+  return(out)
+}
+
+ddir2 <- function(x, prep1, prep2, log = FALSE) {
+  # this function is useful when a discrete uniform hyperprior is imposed on the concentration parameter of the DL prior
+  # every draw of theta which depends upon the concentration parameter, has to be evaluated over a fixed grid of possible values in every iteration
+  # pre calucations of prep1 and prep1 are done in the function BVAR before the posterior sampler starts
+  logd <- colSums(t(prep1) * (log(x))) + prep2
+  if(log == TRUE) out <- logd else if(log == FALSE) out <- exp(logd)
+  return(out)
+}
+
+# Data preparation --------------------------------------------------------
+
+data_prep <- function(Y, p, intercept){
+
+  # Load data
+
+  Yraw <- as.matrix(Y)
+  Traw <- nrow(Yraw) # number of raw observations
+
+  # Create design matrix X and adjust Y
+  # constant (1) plus regressors (lagged values of Y)
+  Ylag <- mlag(Yraw,p)          # Ylag: lagged Y matrix, will be part of X
+
+  if(intercept){
+
+    X <- as.matrix(cbind(Ylag, 1))
+
+  }else X <- Ylag
+
+  Y <- Yraw[(p+1):Traw,]        # delete first lags to match dimensions of X
+
+  return(list(Y=Y,X=X))
+}
+
+mlag <- function(X,p){
+  M<-ncol(X)
+  nr <- nrow(X)-p
+  nc <- p*M
+  Xlag <- matrix(0, nrow = nr,ncol = nc)
+  for (i in 1:(ncol(Xlag)/M)) {
+    Xlag[,(i*M-M+1):(i*M)] <- X[(p-i+1):(nr+p-i),]
+  }
+  Xlag
+}
+
+
+
+# MP Prior functions ------------------------------------------------------
+
+MP_sigma_sq <- function(Y,p, standardize){ # X,
+
+  T <- nrow(Y)
+  M <- ncol(Y)
+  sigma_sq <- rep(as.numeric(NA), M)
+
+  #for(i in 1:M){
+  #  k <- seq(i, p*M, by=M)
+  #  X_i <- X[,k] #c(1,k)
+  #  phi_i <- solve(crossprod(X_i))%*%crossprod(X_i, Y[,i]) # OLS estimates of i-th equation
+  #  sigma_sq[i] <- (crossprod(Y[,i]-X_i%*%phi_i))/(T-p)
+  #}
+  for(i in 1:M){
+
+    Y_i <- as.matrix(Y[,i])
+    X_i <- mlag(Y_i, p)
+    Y_i <- Y_i[-(1:p),]
+    if(standardize){
+      Y_i <- scale(Y_i)
+      X_i <- scale(X_i)
+    }
+    phi_i <- tryCatch(solve(crossprod(X_i))%*%crossprod(X_i, Y_i), error = function(e) solve(qr(X_i, LAPACK = TRUE), Y_i)) # OLS estimates of i-th equation
+    sigma_sq[i] <- (crossprod(Y_i-X_i%*%phi_i))/(T-p)
+
+  }
+  return(sigma_sq)
+}
+
+MP_V_prior_prep <- function(sigma_sq ,K, M, intercept){
+  # sigma_sq: vector of variances of univariate AR(p) models of the individual variables estimated via "MP_sigma_sq"
+  # K: number of regressors per equation
+  # M: number of variables
+
+  V_i <- matrix(0,nrow = K, ncol = M)
+
+  for(i in 1:M){ #for each i-th equation
+    for(j in 1:K){ #for each j-th RHS variable
+      if(j==K & intercept){
+        V_i[j,i] <- sigma_sq[i] #variance on constant
+      }else if((j-i)%%M==0){
+        r <- ceiling((j)/M)
+        V_i[j,i] <- 1/(r^2) #variance on own lags
+      }else{
+        r <- ceiling((j)/M)
+        if(j%%M==0){
+          V_i[j,i] <- (sigma_sq[i])/((r^2)*sigma_sq[(M)])
+        }else if(j%%M==1){
+          V_i[j,i] <- (sigma_sq[i])/((r^2)*sigma_sq[1])
+        }else {
+          V_i[j,i] <- (sigma_sq[i])/((r^2)*sigma_sq[(j%%M)])
+        }
+      }
+    }
+  }
+  return(V_i_prep=as.vector(V_i))
+}
+
+get_MP_V_prior <- function(lambda1=0.04, lambda2=0.0016, lambda3=10^3, sigma_sq ,K, M, intercept){
+  # sigma_sq: vector of variances of univariate AR(p) models of the individual variables estimated via "MP_sigma_sq"
+  # K: number of regressors per equation
+  # M: number of variables
+
+  V_i <- matrix(0,nrow = K, ncol = M)
+
+  for(i in 1:M){ #for each i-th equation
+    for(j in 1:K){ #for each j-th RHS variable
+      if(j==K & intercept){
+        V_i[j,i] <- lambda3 * sigma_sq[i] #variance on constant
+      }else if((j-i)%%M==0){
+        r <- ceiling((j)/M)
+        V_i[j,i] <- lambda1/(r^2) #variance on own lags
+      }else{
+        r <- ceiling((j)/M)
+        if(j%%M==0){
+          V_i[j,i] <- (lambda2 * sigma_sq[i])/((r^2)*sigma_sq[(M)])
+        }else if(j%%M==1){
+          V_i[j,i] <- (lambda2 * sigma_sq[i])/((r^2)*sigma_sq[1])
+        }else {
+          V_i[j,i] <- (lambda2 * sigma_sq[i])/((r^2)*sigma_sq[(j%%M)])
+        }
+      }
+    }
+  }
+  return(V_i=as.vector(V_i))
+}
+
---
+++ R/utility_functions.R
@@ -0,0 +1,202 @@
+# specify Priors ----------------------------------------------------------
+
+specify_PHI_DLprior <- function(theta_dirichlet = c("a" = 0.5),
+                                hyperhyper = FALSE){
+  return(list(theta_dirichlet = theta_dirichlet,
+              hyperhyper=hyperhyper))
+}
+
+specify_PHI_DLprior_new <- function(type = "own/cross", a_1=0.5, a_2=1/10,
+                                    hyperhyper = FALSE){
+  #type: "global": one shrinkage parameter for all coefficients;
+  #"own/cross": seperate shrinkage parameters for own- and crosslags;
+  #"lagwise-own/cross": seperate shrinkage parameters for own- and crosslags for every lag
+  #a_1: if type=="global": global shrinkage parameter;
+  #if type=="own/cross: shrinkage parameter for own lags;
+  #if type=="lagwise-own/cross": shrinkage parameters for own lags, vector of length p;
+  #a_2: if type=="global": redundant (NULL);
+  #if type=="own/cross: shrinkage parameter for cross lags;
+  #if type=="lagwise-own/cross": shrinkage parameters for cross lags, vector of length p;
+  #hyperhyper: discrete uniform hyperprior on the shrinkage hyperparameters: no need to specify a_1 and a_2;
+
+  return(list(type=type,
+              a_1=a_1,
+              a_2=a_2,
+              hyperhyper=hyperhyper))
+}
+
+specify_L_DLprior <- function(theta_dirichlet = c("b" = 0.5),
+                              hyperhyper = FALSE){
+  return(list(theta_dirichlet = theta_dirichlet,
+              hyperhyper=hyperhyper))
+}
+
+specify_PHI_MPprior <- function(lambda_1_gamma = c("shape" = 0.01, "rate" = 0.01),
+                                lambda_2_gamma = c("shape" = 0.01, "rate" = 0.01)){
+  return(list(lambda_1_gamma = lambda_1_gamma,
+              lambda_2_gamma = lambda_2_gamma))
+}
+
+specify_L_MPprior <- function(lambda_4_gamma = c("shape" = 0.01, "rate" = 0.01)){
+  return(list(lambda_4_gamma = lambda_4_gamma))
+}
+
+specify_PHI_SSVSprior <- function(tau_0 = 0.01, tau_1 = 100,
+                                  p_i_beta = c("shape_a" = 0.5, "shape_b" = 0.5),
+                                  semi_automatic = TRUE, df=NULL, rate=1/3,
+                                  k_SA=5, k_AA=5){
+  #df: if PHI_prior == "SSVS-mix", df are the degrees of freedom,
+  # either numeric if wished to be fixed or "hyper" if wished to be treated as random variable
+  #rate: if df == "hyper", rate is the rate of the exponential prior on the degrees of freedom
+  return(list(tau_0 = tau_0,
+              tau_1 = tau_1,
+              p_i_beta = p_i_beta,
+              semi_automatic = semi_automatic,
+              df = df,
+              rate = rate,
+              k_SA = k_SA,
+              k_AA = k_AA))
+}
+
+specify_L_SSVSprior <- function(kappa_0 = 0.001, kappa_1 = 1,
+                                q_i_beta = c("shape_a" = 0.5,
+                                             "shape_b" = 0.5),
+                                df=NULL, rate=1/3,
+                                k_SA=5, k_AA=5){
+  return(list(kappa_0 = kappa_0,
+              kappa_1 = kappa_1,
+              q_i_beta = q_i_beta,
+              df = df,
+              rate = rate,
+              k_SA = k_SA,
+              k_AA = k_AA))
+}
+
+specify_PHI_nonhierarchical <- function(V_prior = 10) {
+  return(V_prior)
+}
+
+specify_L_nonhierarchical <- function(V_prior = 10) {
+  return(V_prior)
+}
+
+pred_eval <- function(h, Y_obs, mod, PHI_draws, L_draws, VoI, SV = TRUE){
+  # Y_obs: ex post observed data for evaluation
+  # mod: model object estimated via BVAR_*
+  # PHI_draws: either posterior draws or savs draws
+  # L_draws: either posterior or savs draws
+  # VoI: variables of interest for joint & marginal predictive likelihoods and MSFE
+
+  # data preparation
+  intercept <- mod$intercept
+  variables <- colnames(mod$Y)
+  draws <- dim(PHI_draws)[1]
+  M <- ncol(mod$Yraw)#############
+  p <- mod$p
+
+  X_fore1 <- mod$X_fore
+
+  if(SV==TRUE) {
+    mus <- mod$Posterior_draws$D$SVpara_draws[,1,]
+    phis <- mod$Posterior_draws$D$SVpara_draws[,2,]
+    sigs <- mod$Posterior_draws$D$SVpara_draws[,3,]
+    h_ts <- mod$Posterior_draws$D$SVlatent_draws[, dim(mod$Posterior_draws$D$SVlatent_draws)[2],]
+  }else if(SV == FALSE){
+    D_draws <- mod$DRAWS$D$D_draws
+  }
+  if(mod$standardize == TRUE){
+    Y_obs <- t((t(Y_obs) - mod$mu_Y)/mod$sd_Y)
+  }
+  Y_obs <- as.matrix(Y_obs, h, M)
+  colnames(Y_obs) <- variables
+
+  # storage
+  PL <- rep(as.numeric(NA), draws)
+  PL_joint <- matrix(as.numeric(NA), draws, h)
+  PL_marginal <- array(as.numeric(NA), c(draws, h, length(VoI)), dimnames = list(NULL, NULL, VoI))
+
+  predictions <- array(as.numeric(NA), c(draws, h, M), dimnames = list(NULL, NULL, variables))
+
+  h_fore <- matrix(as.numeric(NA), M, h)
+  Sigma <- array(as.numeric(NA), c(M,M,h), dimnames = list(variables, variables, NULL))
+
+  for (i in seq.int(draws)) {
+
+    L_inv <- backsolve(L_draws[i,,], diag(M))
+
+    if(SV==TRUE) {
+      # compute k-step ahead forecasts of latent log volas
+      h_fore <- h_ts[i, ]
+      for (k in seq_len(h)) {
+        mu_h <- mus[i,] + phis[i,]*(h_fore - mus[i,])
+        sigma_h <- sigs[i, ]
+        h_fore <- rnorm(M, mean = mu_h, sd= sigma_h)
+
+        # compute SIGMA[t+h]
+        Sigma[,,k] <- t(L_inv) %*% diag(exp(h_fore)) %*% L_inv
+      }
+
+    }else if(SV==FALSE) {
+      # compute SIGMA
+      Sigma[,,1:h] <- t(L_inv) %*% diag(D_draws[i,]) %*% L_inv
+    }
+
+    ## one-steap ahead predictive likelihoods and predictions
+    # predictive mean
+    pred_mean_temp <- as.vector(X_fore1%*%PHI_draws[i,,])
+    names(pred_mean_temp) <- variables
+
+    # Predictive likelihoods
+    PL[i] <- mvtnorm::dmvnorm(as.vector(Y_obs[1,]),pred_mean_temp,Sigma[,,1])
+    PL_joint[i,1] <-  mvtnorm::dmvnorm(as.vector(Y_obs[1,VoI]),pred_mean_temp[VoI],Sigma[VoI,VoI,1])
+    PL_marginal[i,1,] <-  dnorm(as.vector(Y_obs[1, VoI]), pred_mean_temp[VoI], sqrt(diag(Sigma[VoI,VoI,1])))
+
+    # Predictions
+    predictions[i,1,] <- tryCatch(pred_mean_temp + t(chol(Sigma[,,1]))%*%rnorm(M), error=function(e) MASS::mvrnorm(1, pred_mean_temp, Sigma[,,1]))
+
+    ## h-step ahead
+    X_fore_h <- X_fore1
+    if(h>1){
+
+      for (kk in seq_len(h-1)) {
+
+        if(intercept){
+          if(p == 1){
+
+            X_fore_h <- c(predictions[i,kk,],1)
+
+          }else X_fore_h <- c(predictions[i,kk,], X_fore_h[1:(length(X_fore_h)-M-1)],1)
+
+        }else {
+          if(p == 1){
+
+            X_fore_h <- predictions[i,kk,]
+
+          }else X_fore_h <- c(predictions[i,kk,], X_fore_h[1:(length(X_fore_h)-M)])
+
+        }
+
+        pred_mean_temp <- as.vector(X_fore_h%*%PHI_draws[i,,])
+        names(pred_mean_temp) <- variables
+
+        PL_joint[i, kk+1] <-  mvtnorm::dmvnorm(as.vector(Y_obs[kk+1, VoI]),pred_mean_temp[VoI],Sigma[VoI,VoI,kk+1])
+        PL_marginal[i, kk+1,] <-  dnorm(as.vector(Y_obs[kk+1, VoI]), pred_mean_temp[VoI], sqrt(diag(Sigma[VoI,VoI,kk+1])))
+
+        predictions[i, kk+1,] <- tryCatch(pred_mean_temp + t(chol(Sigma[,,kk+1]))%*%rnorm(M), error=function(e) MASS::mvrnorm(1, pred_mean_temp, Sigma[,,kk+1]))
+
+      }
+
+    }
+
+  }
+
+  # MSFE
+  #MSFE <- (colMeans(predictions[,VoI]) - Y_obs[,VoI])^2
+  MSFE <- (apply(predictions[,,VoI, drop=FALSE], 2:3, mean) - Y_obs[,VoI])^2
+
+  return(list(predictions=predictions,
+              LPL=log(mean(PL)),
+              LPL_joint=log(colMeans(PL_joint)),
+              LPL_marginal=log(apply(PL_marginal, 2:3, mean)),
+              MSFE=MSFE))
+}
