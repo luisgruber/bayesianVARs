812a4881e35945b0a79d64690075a906e71faf6e
Gruber <luis.gruber@aau.at>
2022-05-17 21:38:04 +0200

    data_raw

--- data-raw/dat_growth.R
+++ data-raw/dat_growth.R
@@ -2,8 +2,9 @@
 library(tidyverse)
 library(xts)
 library(lubridate)
-dir <- paste0("")
-dat <- readr::read_csv(paste0(dir,"2021-07.csv"))
+dat <- readr::read_csv("https://files.stlouisfed.org/files/htdocs/fred-md/quarterly/2021-07.csv")
+
+type <- "growth" # "stationary"
 
 # remove rows that only contain NAs
 dat <- dat %>% filter(rowSums(is.na(dat)) != ncol(dat))
@@ -15,9 +16,10 @@ data <- as.matrix(dat[-1, c("GDPC1","PCECC96","GPDIC1", "PRFIx", "INDPRO",
                             "FEDFUNDS", "GS1", "GS10", "M2REAL", "EXUSUKx",
                             "UMCSENTx", "S&P 500")])
 
+data_raw <- xts(data[-1,], dates)
 
 # (approx) stationary data ---------------------------------------------------------
-
+if(type=="stationary"){
 # Transform variables according to McCracken & NG (2020): FRED-QD: A Quarterly Database for Macroeconomic Research (https://doi.org/10.20955/wp.2020.005)
 data_trans <- matrix(NA, nrow(data)-1,ncol(data))
 colnames(data_trans) <- colnames(data)
@@ -33,11 +35,11 @@ for (i in 1:ncol(data)) {
 data_trans <- xts(data_trans[-(1:3),], order.by = dates[-(1:3)])
 small <- c("GDPC1", "CPIAUCSL", "FEDFUNDS")
 medium <- c("GDPC1","PCECC96", "GPDIC1", "AWHMAN",  "CPIAUCSL", "CES2000000008x", "FEDFUNDS")
-data_final <- list(small=data_trans[,small], medium=data_trans[,medium], large=data_trans)
-saveRDS(data_final, file = paste0(dir,"data_transformed_new.RData"))
+dat <- list(small=data_trans[,small], medium=data_trans[,medium], large=data_trans)
+}
 
 # Growth data: diff(log) --------------------------------------------------
-
+if(type=="growth"){
 data_growth <- matrix(NA, nrow(data)-1,ncol(data))
 colnames(data_growth) <- colnames(data)
 data[4,"UMCSENTx"] <- data[3,"UMCSENTx"]
@@ -49,25 +51,7 @@ for (i in colnames(data)) {
 data_growth <- xts(data_growth[-(1:3),], order.by = dates[-(1:3)])
 small <- c("GDPC1", "CPIAUCSL", "FEDFUNDS")
 medium <- c("GDPC1","PCECC96", "GPDIC1", "AWHMAN",  "CPIAUCSL", "CES2000000008x", "FEDFUNDS")
-data_final <- list(small=data_growth[,small], medium=data_growth[,medium], large=data_growth)
-saveRDS(data_final, file = paste0(dir,"data_growth.RData"))
-
-
-# Data in log-levels ------------------------------------------------------
-
-# (except interest rates)
-data_llevel <- matrix(NA, nrow(data)-1,ncol(data))
-colnames(data_llevel) <- colnames(data)
-for (i in colnames(data)) {
-  if(i == "FEDFUNDS") data_llevel[,i] <- data[-1,i]
-  else if(i == "GS1") data_llevel[,i] <- data[-1,i]
-  else if(i == "GS10") data_llevel[,i] <- data[-1,i]
-  else data_llevel[,i] <- log(data[-1,i])
+dat_growth <- list(small=data_growth[,small], medium=data_growth[,medium], large=data_growth)
 }
-data_llevel <- xts(data_llevel[-(1:3),], order.by = dates[-(1:3)])
-small <- c("GDPC1", "CPIAUCSL", "FEDFUNDS")
-medium <- c("GDPC1","PCECC96", "GPDIC1", "AWHMAN",  "CPIAUCSL", "CES2000000008x", "FEDFUNDS")
-datall_final <- list(small=data_llevel[,small], medium=data_llevel[,medium], large=data_llevel)
-saveRDS(datall_final, file = paste0(dir,"data_loglevel.RData"))
 
 usethis::use_data(dat_growth, overwrite = TRUE)
