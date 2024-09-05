55cc56442a18eace4d3a3139dc8abaeb32f0a93f
lugruber <luis.gruber@aau.at>
2024-09-05 18:54:57 +0200

    remove redundant tests

--- tests/testthat/test-bvar.R
+++ tests/testthat/test-bvar.R
@@ -95,24 +95,6 @@ test_that("miss-specified input", {
   expect_error(bvar(data, lags = 1L, prior_phi = phi))
 })
 
-test_that("flat prior factor", {
-  data <- usmacro_growth[,c("GDPC1","CPIAUCSL","FEDFUNDS")]
-  phi <- specify_prior_phi(data = data, lags = 2L, prior = "normal",
-                           normal_sds = 1e6)
-  sigma <- specify_prior_sigma(data = data, type = "factor",
-                               factor_factors = 2,
-                               factor_priorfacloadtype = "normal",
-                               factor_priorfacload = 1e6,
-                               factor_heteroskedastic = c(FALSE, FALSE),
-                               factor_priorhomoskedastic = matrix(c(1e-6,1e-6), ncol(data), 2))
- set.seed(123)
-  mod <- bvar(data, lags = 2, prior_intercept = 1e6, prior_phi = phi,
-              prior_sigma = sigma, draws = 10000)
-  phi_post_mean <- apply(mod$PHI, 1:2, mean)
-  ols <- solve(crossprod(mod$X), crossprod(mod$X,mod$Y))
-
-  expect_lt(max(abs(ols-phi_post_mean)),0.01)
-})
 
 test_that("flat prior cholesky", {
   data <- usmacro_growth[,c("GDPC1","CPIAUCSL","FEDFUNDS")]
@@ -132,18 +114,3 @@ test_that("flat prior cholesky", {
   expect_lt(max(abs(ols-phi_post_mean)),0.01)
 })
 
-test_that("homoscedastic factor VAR input", {
-  data <- usmacro_growth[,c("GDPC1","CPIAUCSL","FEDFUNDS")]
-  phi <- specify_prior_phi(data = data, lags = 2L, prior = "normal",
-                           normal_sds = 1e6)
-  sigma <- specify_prior_sigma(data = data, type = "factor",
-                               factor_factors = 2,
-                               factor_priorfacloadtype = "normal",
-                               factor_priorfacload = 1e6,
-                               factor_heteroskedastic = c(FALSE, FALSE),
-                               factor_priorhomoskedastic = matrix(c(1e-6,1e-6), ncol(data), 2))
-
-  mod <- bvar(data, lags = 2, prior_intercept = 1e6, prior_phi = phi,
-              prior_sigma = sigma, draws = 10, burnin = 10)
-  expect_true(all(mod$logvar[,-seq_len(ncol(mod$Y)),]==0))
-})
